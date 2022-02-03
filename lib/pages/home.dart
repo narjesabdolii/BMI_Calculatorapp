94fac31e5bd6e41b73da18df7e65aa12630139c4
narjes_abdoli <abdolizahra909@gmail.com>
2022-02-03 10:13:08 +0330

    changes : adding background image for home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'User.dart';
+import 'dart:ui';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,28 +13,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title : const Text('BMI Calculator',
-          style: TextStyle(
-            fontFamily: 'Dongle-Bold',
-            fontSize: 35.0
-          ) ,),
-        centerTitle: true,
-        backgroundColor: Colors.purple[300],
-      ),
-      body : Column (
+
+      body : Center(
+           child : Container(
+             alignment: const Alignment(-.2, 0),
+           decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/back-2.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      child : Column (
         children: <Widget>[
           Container(
-            margin: const EdgeInsets.fromLTRB(130.0, 50.0, 20.0, 10.0),
+            margin: const EdgeInsets.fromLTRB(60.0, 50.0, 20.0, 10.0),
+            child: const Text(
+              'BMI APP',
+              style: TextStyle(
+                fontSize: 40.0,
+                fontFamily: 'Dongle-Bold',
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(60.0, 40.0, 20.0, 10.0),
             child: const CircleAvatar(
-              radius: 70.0,
+              radius: 85.0,
               backgroundColor: Colors.white,
               backgroundImage:AssetImage('assets/BMI-Calculator.png'),
             ),
           ),
           Container(
-            margin: const EdgeInsets.fromLTRB(130.0, 50.0, 20.0, 10.0),
+            margin: const EdgeInsets.fromLTRB(60.0, 40.0, 20.0, 10.0),
             child: FlatButton(
               onPressed: () {
                 setState(() {
@@ -51,7 +63,7 @@ class _HomeState extends State<Home> {
             ),
           ),
           Container(
-            margin: const EdgeInsets.fromLTRB(130.0, 50.0, 20.0, 10.0),
+            margin: const EdgeInsets.fromLTRB(60.0, 40.0, 20.0, 10.0),
 
             child: FlatButton(
               onPressed: () {
@@ -69,6 +81,8 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
+    ),
+    ),
     );
   }
 }
